81d7452b10ff675976ec98daf6810adfd9695cdf
HuyNguyen7903 <huynguyen7903@gmail.com>
2025-03-08 16:48:11 +0700

    Initial commit - added project files

---
+++ group5.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Danh sách thành viên nhóm 5:");
+        Console.WriteLine("1. Nguyễn Quốc Huy - 2151150041");
+        Console.WriteLine("2. Thái Văn Tĩnh - 2151150063");
+        Console.WriteLine("3. Vũ Hoàng Phúc - 2151150052");
+    }
+}
